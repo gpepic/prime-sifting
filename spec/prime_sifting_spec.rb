20444f1d4924b58bbccc71b2b34b2550135f227e
mark rubenstein & Ginger Pearson <student@epicodus.com>
2014-07-23 10:37:54 -0700

    updated spec and removed 1 as prime

--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -3,6 +3,6 @@ require('prime_sifting')
 
 describe('prime_sifting') do
   it('removes all multiples of 2 that are not the number 2') do
-    prime_sifting(8).should(eq([1, 2, 3, 5, 7]))
+    prime_sifting(8).should(eq([2, 3, 5, 7]))
   end
 end
